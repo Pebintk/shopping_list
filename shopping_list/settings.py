46bf34ddf6df27eb44aa05b0adf938d9496efffe
pebintk <pendar.kasdiono@gmail.com>
2023-11-15 11:40:10 +0700

    until views.py create flutter

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders.middleware.CorsMiddleware',
     'corsheaders',
     'main',
     'authentication',
@@ -57,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'shopping_list.urls'
